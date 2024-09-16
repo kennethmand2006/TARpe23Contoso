be3ec73c60fdf97f38301b072f80a91f994a0a25
Opilane TTHK <opilane@tthk.ee>
2024-09-16 13:29:11 +0300

    muudatused

---
+++ ContosoUniversity/Models/CourseAssignment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class CourseAssignment
+    {
+        [Key]
+        public int ID { get; set; }
+        public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+        public Instructor Instructor { get; set; }
+        public Course Ćourse { get; set; }
+    }
+}
